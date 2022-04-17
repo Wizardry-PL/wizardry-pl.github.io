1b97ed0f2380ea51d9c5ee69b3e02af7e1f409e1
PQCraft <0456523@gmail.com>
2022-04-17 19:57:39 -0400

    CSS and homepage update

--- js/pageload.js
+++ js/pageload.js
@@ -1,6 +1,6 @@
 function pgload() {
-    var divprop1 = '<div id="pgname" style="height: 56px;"><div class="subheader"><b><a class="normcur hlink" style="font-size: 16px; color: var(--font-color);" HREF="">';
-    var divprop2 = '</a></b></div><br><br></div>'
+    var divprop1 = '<div id="pgname" style="height: 56px;"><a class="subheader normcur hlink" style="display: block; color: var(--font-color);" HREF=""><b style="font-size: 16px;">';
+    var divprop2 = '</b></a><br><br></div>'
     if (name != '') {
         document.getElementById("title").outerHTML = '<title id="title">Wizardry: ' + name + '</title>';
         document.getElementById("pgname").outerHTML = divprop1 + '&nbsp;&nbsp;&nbsp;' + name + divprop2;
@@ -8,7 +8,17 @@ function pgload() {
         document.getElementById("title").outerHTML = '<title id="title">Wizardry</title>';
         document.getElementById("pgname").outerHTML = divprop1 + '<br>' + divprop2;
     }
-    if (typeof disableTab === 'undefined' || disableTab != true) {document.getElementsByTagName('body')[0].outerHTML = document.getElementsByTagName('body')[0].outerHTML.replace(/<tb>/g, "&nbsp;&nbsp;&nbsp;&nbsp;");}
     if (document.getElementById("noload") != null) {document.getElementById("noload").outerHTML = '';}
+    customTag("tb", instb);
+}
+function customTag(tagName, fn){
+    document.createElement(tagName);
+    var tagInstances = document.getElementsByTagName(tagName);
+    for (var i = 0; i < tagInstances.length; i++) {
+        fn(tagInstances[i]);
+    }
+}
+function instb(elem) {
+    document.getElementsByTagName('html')[0].innerHTML = document.getElementsByTagName('html')[0].innerHTML.replace(/<tb>/g, "&nbsp;&nbsp;&nbsp;&nbsp;");
 }
 pgload();
